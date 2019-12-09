fcc340ddaef79983d01cd84c126fbf8e7242c131
joharbatta <joharbatta@gmail.com>
2019-12-09 17:38:29 +0530

    Updates

---
+++ precache-manifest.da83ebcd4602647d56f7f1c1b53109d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b2a667ccede57940998d19877e1459",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "04585636309ce0ab9b26",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "826b9b5f9359ed4beefd",
+    "url": "/react-gh-pages/static/js/2.d7cfbedd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.d7cfbedd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04585636309ce0ab9b26",
+    "url": "/react-gh-pages/static/js/main.812ef6ae.chunk.js"
+  },
+  {
+    "revision": "92896a2ba561117d7b15",
+    "url": "/react-gh-pages/static/js/runtime-main.74416f8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
